9f72c8c9614ce356c9cff1a1d02f555afc8deb44
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 18:30:00 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -22,9 +22,9 @@ export default class CardBack extends Component {
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement()}
         <span />
         <h5 className="genres"></h5>
       </div>
